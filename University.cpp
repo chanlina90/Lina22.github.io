7def9e19fb8fb8114ffa3b43d2bc2dd5d02996dc
chanlina90 <165448629+chanlina90@users.noreply.github.com>
2024-04-10 11:18:30 +0700

    Add files via upload

---
+++ University.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x=5;
+	int y=10;
+	
+	
+	cout <<"\nbook = " << (++y%x);
+	cout <<"\npen = " << (y/x);
+	cout <<"\ncolor = " << x*y;
+	cout <<"\nbord = " << (y+y-x);
+	cout <<"\nback = " << (y*10);
+	
+	
+	return 0;
+}
